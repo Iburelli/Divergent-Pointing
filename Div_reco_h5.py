7e3a88a0bf1511965fca3da6f7dab176eea164d3
vuillaut <thomas.vuillaume@gmail.com>
2019-09-20 10:42:11 +0200

    more info in HDF5 files and example

--- Div_reco_h5.py
+++ Div_reco_h5.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import astropy.units as u
 import numpy as np
-import tempfile
+# import tempfile
 from copy import deepcopy
 
 
 import pandas as pd
-from astropy.table import Table
 from astropy.coordinates.angle_utilities import angular_separation
 
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
@@ -18,16 +17,22 @@ from ctapipe.calib import CameraCalibrator
 from ctapipe.reco import HillasReconstructor
 from ctapipe.image import hillas_parameters, leakage
 from ctapipe.image.cleaning import tailcuts_clean
-from ctapipe.visualization import ArrayDisplay
-from ctapipe.image.timing_parameters import timing_parameters
-
-
+# from ctapipe.visualization import ArrayDisplay
+# from ctapipe.image.timing_parameters import timing_parameters
+import tables
+import iof
 import sys
 import os
 
 # Import a simtelarray file of MC simulated data
-datafile = "/Users/alicedonini/PycharmProjects/ctapipe/gamma_20deg_180deg_run875___cta-prod3-demo-2147m-LaPalma-baseline.simtel.gz"
-events = event_source(datafile)
+# input_file = "/Users/alicedonini/PycharmProjects/ctapipe/gamma_20deg_180deg_run875___cta-prod3-demo-2147m-LaPalma-baseline.simtel.gz"
+input_file = sys.argv[1]
+output_filename = ('dl1_' + os.path.basename(input_file).split('.')[0] + '.h5')
+
+
+
+
+events = event_source(input_file, back_seekable=True, max_events=30)
 
 calibrator = CameraCalibrator()
 
@@ -35,12 +40,41 @@ location = EarthLocation.of_site('Roque de los Muchachos')
 obstime = Time('2018-11-01T02:00')
 horizon_frame = AltAz(location=location, obstime=obstime)
 
-cleaning_level = {'LSTCam': (4, 8, 2),
-                'NectarCam': (5, 10, 2)}
+cleaning_level = {
+    'LSTCam': (4, 8, 2),
+    'NectarCam': (5, 10, 2),
+    'FlashCam': (4, 8, 2),
+    'std': (4, 8, 2)
+}
 
-with HDF5TableWriter(filename='hillas.h5', group_name='events', mode='w') as writer:
 
-    for event in events:
+
+filters = tables.Filters(
+    complevel=5,    # enable compression, with level 0=disabled, 9=max
+    complib='blosc:zstd',   #  compression using blosc
+    fletcher32=True,    # attach a checksum to each chunk for error correction
+    bitshuffle=False,   # for BLOSC, shuffle bits for better compression
+)
+
+
+
+
+### Write extra information to the DL1 file
+event = next(iter(events))
+metadata = iof.global_metadata(events)
+iof.write_metadata(metadata, output_filename)
+iof.write_array_info(event, output_filename)
+iof.write_mcheader(event.mcheader, output_filename, obs_id=event.r0.obs_id, filters=filters, metadata=metadata)
+
+
+extra_im_c = iof.ExtraImageInfo()
+event_info_c = iof.EventInfo()
+
+with HDF5TableWriter(filename=output_filename, group_name='events', mode='a') as writer:
+
+    for ii, event in enumerate(events):
+        if ii%1 == 0:
+            print(ii)
         calibrator(event)
         if len(event.r0.tels_with_data) < 2:
             continue
@@ -54,11 +88,21 @@ with HDF5TableWriter(filename='hillas.h5', group_name='events', mode='w') as wri
         telescope_pointings = {}
 
         for telescope_id, dl1 in event.dl1.tel.items():
+
+            tel = event.dl1.tel[telescope_id]
+            tel.prefix = ''  # don't really need one for HDF5 writer
+            extra_im_c.tel_id = telescope_id
+            extra_im_c.event_id = event.r0.event_id
+            extra_im_c.obs_id = event.r0.obs_id
+
             camera = event.inst.subarray.tels[telescope_id].camera
             image = dl1.image
             #pulse_time = dl1.pulse_time
 
-            boundary, picture, min_neighbors = cleaning_level[camera.cam_id]
+            if camera.cam_id in cleaning_level:
+                boundary, picture, min_neighbors = cleaning_level[camera.cam_id]
+            else:
+                boundary, picture, min_neighbors = cleaning_level['std']
 
             # create a clean mask of pixels above the threshold
             clean = tailcuts_clean(
@@ -100,6 +144,10 @@ with HDF5TableWriter(filename='hillas.h5', group_name='events', mode='w') as wri
             telescope_pointings[telescope_id] = SkyCoord(alt=event.mc.tel[telescope_id].altitude_raw * u.rad,
                                                          az=event.mc.tel[telescope_id].azimuth_raw * u.rad,
                                                          frame=horizon_frame)
+
+            # remove the first part of the tel_name which is the type 'LST', 'MST' or 'SST'
+            tel_name = str(event.inst.subarray.tel[telescope_id])[4:]
+            writer.write(table_name='dl1', containers=[hillas_c, leakage_c, extra_im_c])
             
             # Camera display
 
@@ -119,21 +167,33 @@ with HDF5TableWriter(filename='hillas.h5', group_name='events', mode='w') as wri
         # array pointing needed for the creation of the TiltedFrame to perform the impact point reconstruction
         array_pointing = SkyCoord(az=event.mc.az, alt=event.mc.alt, frame=horizon_frame)
 
+        event_info_c.event_id = event.r0.event_id
+        event_info_c.obs_id = event.r0.obs_id
+
         if len(hillas_dict) > 1:
             stereo = reco.predict(
                 hillas_dict, event.inst, array_pointing, telescope_pointings
             )
-            writer.write('reconstructed', stereo)
-            writer.write('true', event.mc)
+
+            writer.write(table_name='reconstructed',
+                         containers=[stereo, event_info_c])
+            writer.write(table_name='true', containers=[event.mc, event_info_c])
 
         # save event for plotting later
-        if event.count == 3:
-            plotting_event = deepcopy(event)
-            plotting_hillas = hillas_dict
-            #plotting_timing = time_gradients
-            plotting_stereo = stereo
+        # if event.count == 3:
+        #     plotting_event = deepcopy(event)
+        #     plotting_hillas = hillas_dict
+        #     #plotting_timing = time_gradients
+        #     plotting_stereo = stereo
 
-angle_offset = plotting_event.mcheader.run_array_direction[0]
+
+# thrown the simtel energy histogram needs to loop over all the simtel file so we don't dump in case of tests
+if events.max_events is None:
+    iof.write_simtel_energy_histogram(events, output_filename, obs_id=event.dl0.obs_id, metadata=metadata)
+
+
+
+# angle_offset = plotting_event.mcheader.run_array_direction[0]
 
 # Array display
 # disp = ArrayDisplay(plotting_event.inst.subarray)
@@ -158,8 +218,8 @@ angle_offset = plotting_event.mcheader.run_array_direction[0]
 # plt.ylim(-400, 400)
 
 # theta-square plot
-df_rec = pd.read_hdf("hillas.h5", key='events/reconstructed')
-df_true = pd.read_hdf("hillas.h5", key='events/true')
+df_rec = pd.read_hdf(output_filename, key='events/reconstructed')
+df_true = pd.read_hdf(output_filename, key='events/true')
 
 # get angular offset between reconstructed shower direction and MC
 # generated shower direction
@@ -169,6 +229,6 @@ theta = angular_separation(
 )
 
 plt.hist(theta.to(u.deg).value**2, bins=25, range=[0, 0.3])
-plt.xlabel(r'$\theta² / deg²$')
+plt.xlabel(r'$\theta2 / deg2$')
 plt.ylabel("# of events")
 plt.show()
