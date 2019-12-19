f5fb6de9d4e11002af18e645b5ac3109ecdbee1f
Alice Donini <alixd90@gmail.com>
2019-12-19 16:58:48 +0100

    small add

--- divtel/divtel/pointing.py
+++ divtel/divtel/pointing.py
@@ -19,30 +19,35 @@ def retro_pointing(array, div, alt, az):
     return np.array([Gx, Gy, Gz])
 
 
-def tel_divergent_pointing(array, tel, div, alt, az):
-    G = retro_pointing(array, div, alt, az)
+def tel_div_pointing(tel, G):
     GT = np.sqrt(((tel - G) ** 2).sum())
-    #         print("GT", GT)
     alt_tel = np.arcsin((tel[2] - G[2]) / GT)
     az_tel = np.arctan2((tel[1] - G[1]), (tel[0] - G[0]))
     #         print("alt", np.rad2deg(alt_tel))
     #         print("az", az_tel)
     return 90 - np.rad2deg(alt_tel), np.rad2deg(np.mod(az_tel, 2 * np.pi))
 
+def array_div_pointing(array, G):
+    pointings = []
+    for ii, tel in enumerate(array.positions_array):
+        theta, phi = tel_div_pointing(tel, G)
+        print("theta: ", theta)
+        print("phi: ", phi)
 
 def main():
     tel1 = Telescope(0 * u.m, 0 * u.m, 0 * u.m, 28 * u.m, 1 * u.m)
     tel2 = Telescope(1 * u.m, 0 * u.m, 0 * u.m, 28 * u.m, 1 * u.m)
     tel3 = Telescope(0 * u.m, 1 * u.m, 0 * u.m, 28 * u.m, 1 * u.m)
 
-    array = Array([tel1, tel2, tel3])
-    tels_pos = array.positions_array
-    print(array.positions_array)
+    div = 1e-15
+    alt = np.deg2rad(90)
+    az = np.pi
 
-    for ii, tel in enumerate(tels_pos):
-        theta, phi = tel_divergent_pointing(array, tel, 1e-15, np.deg2rad(90), np.pi)
-        print("theta: ", theta)
-        print("phi: ", phi)
+    array = Array([tel1, tel2, tel3])
+    G = retro_pointing(array, div, alt, az)
+
+    array_div_pointing(array, G)
+    
 
 if __name__ == "__main__":
     main()
